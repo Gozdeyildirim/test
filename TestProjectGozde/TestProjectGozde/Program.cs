7dab091fb6a6dc3505a851ad31692d0cfb836b5a
Gozdeyildirim <gozde.yildirim@turkishexporter.net>
2020-11-23 14:53:33 +0300

    test

--- TestProjectGozde/TestProjectGozde/Program.cs
+++ TestProjectGozde/TestProjectGozde/Program.cs
@@ -6,7 +6,31 @@ namespace TestProjectGozde
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! fdf");
+            int s1, s2, sonuc, cevap, dsayisi = 0, ysayisi = 0;
+            Random rnd = new Random();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                s1 = rnd.Next(1, 10);
+                s2 = rnd.Next(1, 10);
+                sonuc = s1 * s2;
+                Console.Write(s1 + "x" + s2 + "= ");
+                cevap = Convert.ToInt32(Console.ReadLine());
+                if (cevap == sonuc)
+                {
+                    Console.WriteLine("Cevap Doğru");
+                    dsayisi++; //www.yazilimkodlama.com
+                }
+                else
+                {
+                    Console.WriteLine("Cevap Yanlış");
+                    ysayisi++;
+                }
+
+            }  //www.yazilimkodlama.com
+            Console.WriteLine();
+            Console.WriteLine("Doğru Cevap Sayısı : {0}   -   Yanlış Cevap Sayısı : {1}", dsayisi, ysayisi);
+            Console.ReadKey();
         }
     }
 }
