ed4c25eba85db0c11180a211569853cc77e02fa4
Gozdeyildirim <gozde.yildirim@turkishexporter.net>
2020-11-23 13:11:48 +0300

    testtt

---
+++ TestProjectGozde/TestProjectGozde/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestProjectGozde
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! fdf");
+        }
+    }
+}
