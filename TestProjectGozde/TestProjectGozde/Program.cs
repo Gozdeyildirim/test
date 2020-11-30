96849eb187e686a1e8fa4b46c30b2faacd51b036
Gozdeyildirim <gozde.yildirim@turkishexporter.net>
2020-11-30 09:11:44 +0300

    Girilen Cümledeki Kelime Sayısını Bulma

--- TestProjectGozde/TestProjectGozde/Program.cs
+++ TestProjectGozde/TestProjectGozde/Program.cs
@@ -9,47 +9,16 @@ namespace TestProjectGozde
             //kodları hep buraya yazın static void main içine hata verir yoksa
 
 
-            int mak = 0;
-            int son = 0;
-            Console.WriteLine("Cümleyi Girin :");
-            Console.WriteLine("==========================================");
-            String metin = Console.ReadLine();
-            metin = metin.ToLower();
-            String karakterler = "0123456789abcdefghijklmnopqrstuvwxyz.?,;";
-            int[] count = new int[karakterler.Length];
-            for (int i = 0; i < metin.Length; i++)
+            string metin;
+            Console.Write("Metni Girin  : ");
+            metin = Console.ReadLine();
+            string[] kelimeler = metin.Split(' ');
+            Console.WriteLine("Kelime Sayısı :" + kelimeler.Length);
+            foreach (string kelime in kelimeler)
             {
-                int index = karakterler.IndexOf(metin[i]);
-                if (index < 0)
-                    continue;
-                else
-                {
-                    count[index]++;
-                }
+                Console.WriteLine(kelime);
             }
-            for (int i = 0; i < count.Length; i++)
-            {
-                if (count[i] < 1)
-                    continue;
-                else
-                {
-                    Console.WriteLine(karakterler[i] + " " + count[i]);
-                }
-            }
-
-            //En fazla bulunan harf sayısı
-            for (int i = 1; i < count.Length; i++)
-            {
-                if (mak < count[i])
-                {
-                    mak = count[i];
-                    son = i; //Harf sayısı en fazla olan index numarası
-                }
-
-            }
-            Console.WriteLine("En fazla olan karakter : " + karakterler[son] + ", sayısı " + mak);
             Console.ReadKey();
-
 
 
         }
